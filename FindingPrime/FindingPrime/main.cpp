e63ec0c2186eaa8191875029804683b23cb86f21
Chang Yu-hsiang <yu-hsiangchang@Yu-hsiangdeMacBook-Pro.local>
2017-06-30 03:15:42 +0800

    ver1.0

--- FindingPrime/FindingPrime/main.cpp
+++ FindingPrime/FindingPrime/main.cpp
@@ -12,11 +12,40 @@ using namespace std;
 
 int main() {
 
-    int n ;
+    int n, i, j, k, m;
     
     cin>>n;
+    
+    int prime[n+1];
+    
+    prime[1] = 0;
+    prime[2] = 2;
+    
+    for(i = 3 ; i <= n ; i = i + 2 ){
+        
+        for( j = 2 ; j < i ; j++ )
+            
+            if(i % j == 0){
+                prime[i] = 0;
+                break;
+            }
+        
+            else prime[i] = i;
+    }
+    
+    for( m = 4 ; m <= n ; m = m + 2){
+        
+        prime[m] = 0;
+    }
+    
+    for ( k = 1 ; k <= n ; k++){
+        
+        cout << prime[k] << " ";
+        
+    }
     
     cout << endl;
     
     return 0;
+    
 }
